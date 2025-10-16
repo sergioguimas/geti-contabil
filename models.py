91fbc7b860f98fe036a61239c5ba4de8ad27d8de
João Artur <164247347+joaoartur-mk0@users.noreply.github.com>
2025-10-16 10:32:40 -0300

    Correção hash de senha - diminuição de variável

--- models.py
+++ models.py
@@ -56,9 +56,9 @@ def cadastro_contador(NOME, EMAIL, SENHA):
             print("Email já cadastrado no sistema!")
         else:
             if valida_email(EMAIL) == True:
-                senha_hash = generate_password_hash(SENHA)
+                SENHA = generate_password_hash(SENHA)
                 SQL_INSERT = "INSERT INTO contador (nome, email, senha_hash) VALUES(?, ?, ?);"
-                VALUES = (NOME, EMAIL, senha_hash)
+                VALUES = (NOME, EMAIL, SENHA)
                 SQL.execute(SQL_INSERT, VALUES)
                 CONN.commit()
                 DATA_LOG = {'nome':NOME, 'email':EMAIL}
